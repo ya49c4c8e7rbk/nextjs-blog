d51fec6351e543e1a33684a1150a0fce01bddf36
ya49c4c8e7rbk <ya49c4c8e7rbk@gmail.com>
2020-11-01 20:21:20 +0900

    namifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export const siteTitle = 'Next.js Sample Website'
-
 export default class extends Document {
   static async getInitialProps(ctx: any): Promise<any> {
     const sheet = new ServerStyleSheet()
@@ -36,19 +34,6 @@ export default class extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="Learn how to build a personal website using Next.js"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.now.sh/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <title>{siteTitle}</title>
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import { getSortedPostsData } from '@/lib/posts'
 import Layout from '@/components/layout'
 import Date from '@/components/date'
 import utilStyles from '@/styles/utils.module.css'
+import Head from 'next/Head'
+
+export const siteTitle = 'Next.js Sample Website'
 
 export default function Home({
   allPostsData,
@@ -16,6 +19,21 @@ export default function Home({
 }): JSX.Element {
   return (
     <Layout home>
+      <Head>
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="og:title" content={siteTitle} />
+        <title>{siteTitle}</title>
+      </Head>
       <section className={utilStyles.headingMd}>
         <div>
           <div>ITエンジニア 8 年目</div>
