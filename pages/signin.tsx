f2b30f50ecbebd9908520f22e701b5bc337ef1ab
ya49c4c8e7rbk <ya49c4c8e7rbk@gmail.com>
2020-11-03 14:05:49 +0900

    test

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -3,7 +3,7 @@ import FirebaseAuth from '@/components/FirebaseAuth'
 export default function Auth(): React.ReactElement {
   return (
     <div>
-      <p>Sign in</p>
+      <p>Sign in!</p>
       <div>
         <FirebaseAuth />
       </div>
